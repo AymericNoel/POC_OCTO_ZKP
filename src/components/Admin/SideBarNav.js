a109ce435808a607284be3a4af9b13f9ffb78f51
Aymeric.Noel <aym.noel@gmail.com>
2021-04-12 14:55:16 +0200

    cleanup : remove uncessary element from sideNavs

--- src/components/Admin/SideBarNav.js
+++ src/components/Admin/SideBarNav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBBtn, MDBCollapse, MDBNav, MDBNavItem, MDBInput } from 'mdbreact';
+import { MDBBtn, MDBCollapse, MDBNav, MDBInput } from 'mdbreact';
 
 class SideBarNav extends Component {
   constructor() {
@@ -19,87 +19,82 @@ class SideBarNav extends Component {
     const { collapseID } = this.state;
     return (
       <MDBNav className='flex-column'>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse1')}
+            className='w-100'
+          >
+            Accepter un jardin
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse1' isOpen={collapseID}>
+          <form>
+            <MDBInput
+              className='text-center'
+              label='Id du jardin'
+              validate
               size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse1')}
-              className='w-100'
-            >
-              Accepter un jardin
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse1' isOpen={collapseID}>
-            <form>
-              <MDBInput className='text-center' label='Id du jardin' validate size='sm' />
-              <MDBInput label='Preuve ZKP' validate size='sm' />
-              <div className='text-center mb-2'>
-                <MDBBtn size='sm'>Send</MDBBtn>
-              </div>
-            </form>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse2')}
-              className='w-100'
-            >
-              Rejeter un jardin
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse2' isOpen={collapseID}>
-            <p>button 2</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse4')}
-              className='w-100'
-            >
-              Valider un litige
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse4' isOpen={collapseID}>
-            <p>button 2</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse3')}
-              className='w-100'
-            >
-              Fixer redistribution pour litige
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse3' isOpen={collapseID}>
-            <p>button 2</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse5')}
-              className='w-100'
-            >
-              Modifier l&apos;addresse du contrat verifieur
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse5' isOpen={collapseID}>
-            <p>verifier</p>
-          </MDBCollapse>
-        </MDBNavItem>
+            />
+            <MDBInput label='Preuve ZKP' validate size='sm' />
+            <div className='text-center mb-2'>
+              <MDBBtn size='sm'>Send</MDBBtn>
+            </div>
+          </form>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse2')}
+            className='w-100'
+          >
+            Rejeter un jardin
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse2' isOpen={collapseID}>
+          <p>button 2</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse4')}
+            className='w-100'
+          >
+            Valider un litige
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse4' isOpen={collapseID}>
+          <p>button 2</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse3')}
+            className='w-100'
+          >
+            Fixer redistribution pour litige
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse3' isOpen={collapseID}>
+          <p>button 2</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse5')}
+            className='w-100'
+          >
+            Modifier l&apos;addresse du contrat verifieur
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse5' isOpen={collapseID}>
+          <p>verifier</p>
+        </MDBCollapse>
       </MDBNav>
     );
   }
--- src/components/OwnerRents/OwnerRentsSideNav.js
+++ src/components/OwnerRents/OwnerRentsSideNav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBBtn, MDBCollapse, MDBNav, MDBNavItem } from 'mdbreact';
+import { MDBBtn, MDBCollapse, MDBNav } from 'mdbreact';
 
 class OwnerRentsSideNav extends Component {
   constructor() {
@@ -19,111 +19,97 @@ class OwnerRentsSideNav extends Component {
     const { collapseID } = this.state;
     return (
       <MDBNav className='flex-column'>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse1')}
-              className='w-100'
-            >
-              Changer le contact
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse1' isOpen={collapseID}>
-            <p>ter</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse2')}
-              className='w-100'
-            >
-              Changer le secret
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse2' isOpen={collapseID}>
-            <p>button 2</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse4')}
-              className='w-100'
-            >
-              Ajouter une proposition
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse4' isOpen={collapseID}>
-            <p>button 2</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse7')}
-              className='w-100'
-            >
-              Supprimer une proposition
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse7' isOpen={collapseID}>
-            <p>button 2</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse3')}
-              className='w-100'
-            >
-              Ajouter un code d&apos;accès
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse3' isOpen={collapseID}>
-            <p>button 2</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse5')}
-              className='w-100'
-            >
-              Ouvrir un litige
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse5' isOpen={collapseID}>
-            <p>verifier</p>
-          </MDBCollapse>
-        </MDBNavItem>
-        <MDBNavItem>
-          <div className='text-center mb-2'>
-            <MDBBtn
-              size='sm'
-              color='success'
-              onClick={this.toggleCollapse('collapse6')}
-              className='w-100'
-            >
-              Retirer ses éthers après location
-            </MDBBtn>
-          </div>
-          <MDBCollapse id='collapse6' isOpen={collapseID}>
-            <p>verifier</p>
-          </MDBCollapse>
-        </MDBNavItem>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse1')}
+            className='w-100'
+          >
+            Changer le contact
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse1' isOpen={collapseID}>
+          <p>ter</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse2')}
+            className='w-100'
+          >
+            Changer le secret
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse2' isOpen={collapseID}>
+          <p>button 2</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse4')}
+            className='w-100'
+          >
+            Ajouter une proposition
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse4' isOpen={collapseID}>
+          <p>button 2</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse7')}
+            className='w-100'
+          >
+            Supprimer une proposition
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse7' isOpen={collapseID}>
+          <p>button 2</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse3')}
+            className='w-100'
+          >
+            Ajouter un code d&apos;accès
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse3' isOpen={collapseID}>
+          <p>button 2</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse5')}
+            className='w-100'
+          >
+            Ouvrir un litige
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse5' isOpen={collapseID}>
+          <p>verifier</p>
+        </MDBCollapse>
+        <div className='text-center mb-2'>
+          <MDBBtn
+            size='sm'
+            color='success'
+            onClick={this.toggleCollapse('collapse6')}
+            className='w-100'
+          >
+            Retirer ses éthers après location
+          </MDBBtn>
+        </div>
+        <MDBCollapse id='collapse6' isOpen={collapseID}>
+          <p>verifier</p>
+        </MDBCollapse>
       </MDBNav>
     );
   }
